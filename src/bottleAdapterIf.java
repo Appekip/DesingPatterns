4301fdf529009c8423a6120663f851554b980d6e
Arttu <arttu.peramaki@gmail.com>
2021-12-02 12:00:12 +0200

    walmis

---
+++ src/bottleAdapterIf.java
@@ -0,0 +1,5 @@
+public interface bottleAdapterIf {
+
+    double volume();
+
+}
---
+++ src/waterBottle.java
@@ -0,0 +1,9 @@
+public class waterBottle implements bottle{
+
+    double v = 0.5;
+
+    @Override
+    public double volume() {
+        return v;
+    }
+}
